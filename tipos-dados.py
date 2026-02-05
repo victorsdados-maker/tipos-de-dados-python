183dbc2780b8b7d667a76d1ffad3ede849c5110c
victorsdados-maker <victor.s.dados@gmail.com>
2026-02-05 18:04:57 -0300

    Primeiro upload de projeto

---
+++ tipos-dados.py
@@ -0,0 +1,24 @@
+#Exemplos de entrada dos 4 tipos de dados e saída com print.
+#Conceitos usados:str simples(sem uso de métodos),int,float,bool e print
+#OBS:Caso queira printar o valor da variável junto com um texto adicional ou outros valores é necessário uso de  f-string.
+
+#str,texto
+nome=input("Qual seu nome?")
+print(nome)
+
+#int,inteiro
+idade=int(input("Qual sua idade?"))
+print(idade)
+
+#float,decimal
+altura=float(input("Qual sua altura em metros?"))
+print(altura)
+
+#bool(valores booleanos apresentam apenas duas posiibilidades: True ou False)
+x = True
+y = False
+
+print(x)   # saída: True
+print(y)   # saída: False		#Exemplo foi dado sem input para simplificar.
+
+
